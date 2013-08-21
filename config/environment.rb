3edf9f3c00543a2d37f6b812ba375cd5bb7566e3
Daniel Cleary <dan.cleary@sendgrid.com>
2013-08-21 10:41:46 -0700

    Edited environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 SampleApp::Application.initialize!
 
-ActionMailer::Base.stmp_settings = {
-	:user_name => 'dan.cleary@sendgrid.com',
-	:password => 'Polkmnji7',
-	:domain => '#',
-	:address => 'smtp.sendgrid.net'
-	:port =>  587,
-	:authentication => :plain
-	:enable_starttls_auto => true
-}
+#ActionMailer::Base.stmp_settings = {
+#	:user_name => 'dan.cleary@sendgrid.com',
+#	:password => 'Polkmnji7',
+#	:domain => '#',
+#	:address => 'smtp.sendgrid.net'
+#	:port =>  587,
+#	:authentication => :plain
+#	:enable_starttls_auto => true
+#}
