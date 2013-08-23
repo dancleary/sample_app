1bc0814539e28e683318f5fd48aa9978bcb4f5ed
Daniel Cleary <dan.cleary@sendgrid.com>
2013-08-23 11:02:51 -0700

    Fixed Delete account action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
  
 
   private
